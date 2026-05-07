6d8fd5f34b3a25f04ebd32b2c5bc78b403c78e36
Jhampier Jhordch Ortiz Vento <jhampierjhordchortizvento@gmail.com>
2026-05-07 01:11:30 -0500

    Update db.js

--- backend-yape-store/config/db.js
+++ backend-yape-store/config/db.js
@@ -1,11 +1,21 @@
-const mysql = require('mysql2/promise');
-require('dotenv').config();
+const mysql = require('mysql2'); // Usamos la librería base
 
+// Creamos el pool de forma tradicional
 const pool = mysql.createPool({
-    host: 'mysql-118092dd-jortizvento-9349.b.aivencloud.com',
-    user: 'avnadmin',
-    password: 'TU_PASSWORD_REAL_AQUÍ',
-    database: 'defaultdb',
-    port: 28944,
-    ssl: { rejectUnauthorized: false }
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
+
+// ESTO ES LO MÁS IMPORTANTE: Convertimos el pool a promesas manualmente
+const promisePool = pool.promise();
+
+module.exports = promisePool; // Exportamos el pool que SI tiene .query()
